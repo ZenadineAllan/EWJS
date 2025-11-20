b749ffabb4be1f4581e197df9322003b241d3ef6
Servant of the Lord Jesus Christ <zenadineallan7@gmail.com>
2025-11-21 02:28:08 +0300

    Delete homepage directory

--- homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'homepage'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('newstudent/', views.newstudent, name='newstudent')
-]
